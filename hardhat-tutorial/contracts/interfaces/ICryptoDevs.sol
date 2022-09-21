1130bfa69f792bbd3e041d7e36206d2be65257a6
simutis <54304022+SimasG@users.noreply.github.com>
2022-09-22 01:47:46 +0300

    added frontend logic in my-app

--- hardhat-tutorial/contracts/interfaces/ICryptoDevs.sol
+++ hardhat-tutorial/contracts/interfaces/ICryptoDevs.sol
@@ -6,9 +6,11 @@ interface ICryptoDevs {
      * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
      * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
      */
-    // These funcs are coming from `ERC721Enumerable` contract imported in `CryptoDevs` contract
+    // This func comes from `ERC721Enumerable` contract imported in `CryptoDevs` (NFT) contract
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
 
-    // ** Don't we also have a `balanceOf` func from `ERC20` Contract?
+    // ** How do I know if this func comes from th `ERC721Enumable` or `ERC20` contract?
+    // ** ERC721: "function balanceOf(address owner) public view virtual override returns (uint256) {}"
+    // ** ERC20: "function balanceOf(address account) public view virtual override returns (uint256) {}"
     function balanceOf(address owner) external view returns (uint256 balance);
 }
